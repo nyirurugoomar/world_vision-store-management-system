da647e355991ecda1234a02f9ab7dd8ae2cdf279
Omar <nyirurugoomar@gmail.com>
2024-12-23 22:06:42 +0200

    Adding page and make routes for pages

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './components/login/login.component';
+import { TechnicianLoginComponent } from './components/technician-login/technician-login.component';
+import { StoreKeeperLoginComponent } from './components/store-keeper-login/store-keeper-login.component';
 
 const routes: Routes = [
-  {path: '', component:LoginComponent}
+  {path: '', component:LoginComponent},
+  {path: 'app-technician-login', component:TechnicianLoginComponent},
+  {path: 'app-store-keeper-login', component:StoreKeeperLoginComponent},
 ];
 
 @NgModule({
